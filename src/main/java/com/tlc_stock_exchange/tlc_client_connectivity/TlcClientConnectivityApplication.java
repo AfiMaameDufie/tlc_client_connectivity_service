d734ad1b067fc8499b54a98839614ab3e3351d17
Afi Maame Dufie <afimaamedufie@gmail.com>
2020-11-17 14:41:47 +0000

    Initial Commit

---
+++ src/main/java/com/tlc_stock_exchange/tlc_client_connectivity/TlcClientConnectivityApplication.java
@@ -0,0 +1,13 @@
+package com.tlc_stock_exchange.tlc_client_connectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlcClientConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlcClientConnectivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tlc_stock_exchange/tlc_client_connectivity/TlcClientConnectivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlc_stock_exchange.tlc_client_connectivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlcClientConnectivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
